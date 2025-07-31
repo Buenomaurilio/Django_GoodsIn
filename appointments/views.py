8815b022d597f7a893edad64d40443843420a26f
Maurilio Bueno <53921060+Buenomaurilio@users.noreply.github.com>
2025-07-31 20:51:33 +0100

    Update views.py

--- appointments/views.py
+++ appointments/views.py
@@ -1,32 +1,24 @@
-from django.utils.dateparse import parse_date
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Appointment
-from .forms import AppointmentForm, CheckerForm
-from .models import Checker
-from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
-from datetime import time, datetime
-import csv
-import io
-from django.http import HttpResponse
-from django.contrib import messages
-from django import forms
 from django.utils.dateparse import parse_date, parse_time
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Appointment, Checker
+from .forms import AppointmentForm, CheckerForm
+from django.contrib.auth.decorators import login_required
 from django.db.models import Sum, Count
 from django.db.models.functions import TruncDay, TruncMonth, TruncWeek
 from django.utils.timezone import now
-from django.shortcuts import render
-from .models import Appointment
-
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
+from django.contrib import messages
+from django import forms
+import csv
+import io
 
 class CSVImportForm(forms.Form):
     file = forms.FileField()
 
-
 @login_required
 def appointment_list(request):
     data_filter = request.GET.get('date')
-
     if data_filter:
         appointments = Appointment.objects.filter(scheduled_date=data_filter).order_by('scheduled_time')
     else:
@@ -35,23 +27,16 @@ def appointment_list(request):
     morning_shift = appointments.filter(scheduled_time__gte=time(5, 0), scheduled_time__lt=time(13, 30))
     back_shift = appointments.filter(scheduled_time__gte=time(13, 0), scheduled_time__lt=time(21, 30))
 
-    total_appointments = appointments.count()
-    morning_count = morning_shift.count()
-    back_count = back_shift.count()
-
-    morning_pallets = morning_shift.aggregate(Sum('qtd_pallet'))['qtd_pallet__sum'] or 0
-    back_pallets = back_shift.aggregate(Sum('qtd_pallet'))['qtd_pallet__sum'] or 0
-
-    return render(request, 'appointments/appointment_list.html', {
+    context = {
         'appointments': appointments,
         'data_filter': data_filter,
-        'total_appointments': total_appointments,
-        'morning_count': morning_count,
-        'back_count': back_count,
-        'morning_pallets': morning_pallets,
-        'back_pallets': back_pallets,
-    })
-
+        'total_appointments': appointments.count(),
+        'morning_count': morning_shift.count(),
+        'back_count': back_shift.count(),
+        'morning_pallets': morning_shift.aggregate(Sum('qtd_pallet'))['qtd_pallet__sum'] or 0,
+        'back_pallets': back_shift.aggregate(Sum('qtd_pallet'))['qtd_pallet__sum'] or 0,
+    }
+    return render(request, 'appointments/appointment_list.html', context)
 
 @login_required
 def edit_appointment(request, pk):
@@ -60,15 +45,13 @@ def edit_appointment(request, pk):
         form = AppointmentForm(request.POST, instance=appointment)
         if form.is_valid():
             form.save()
+            date_param = request.GET.get('date')
+            if date_param:
+                return redirect(f"{reverse('appointment_list')}?date={date_param}")
             return redirect('appointment_list')
     else:
         form = AppointmentForm(instance=appointment)
-
-    return render(request, 'appointments/edit_appointment.html', {
-        'form': form,
-        'appointment': appointment
-    })
-
+    return render(request, 'appointments/edit_appointment.html', {'form': form, 'appointment': appointment})
 
 @login_required
 def add_appointment(request):
@@ -76,11 +59,13 @@ def add_appointment(request):
         form = AppointmentForm(request.POST)
         if form.is_valid():
             form.save()
+            date_param = request.GET.get('date')
+            if date_param:
+                return redirect(f"{reverse('appointment_list')}?date={date_param}")
             return redirect('appointment_list')
     else:
         form = AppointmentForm()
     return render(request, 'appointments/add_appointment.html', {'form': form})
-
 
 @login_required
 def add_checker(request):
@@ -88,11 +73,13 @@ def add_checker(request):
         form = CheckerForm(request.POST)
         if form.is_valid():
             form.save()
+            date_param = request.GET.get('date')
+            if date_param:
+                return redirect(f"{reverse('appointment_list')}?date={date_param}")
             return redirect('appointment_list')
     else:
         form = CheckerForm()
     return render(request, 'appointments/add_checker.html', {'form': form})
-
 
 @login_required
 def export_appointments_csv(request):
@@ -122,7 +109,6 @@ def export_appointments_csv(request):
         ])
 
     return response
-
 
 @login_required
 def import_appointments_csv(request):
@@ -156,16 +142,12 @@ def import_appointments_csv(request):
 
     return render(request, 'appointments/import_csv.html', {'form': form})
 
-
 @login_required
 def delete_appointment(request, pk):
     appointment = get_object_or_404(Appointment, pk=pk)
     appointment.delete()
     messages.success(request, "Appointment deleted successfully.")
     return redirect('appointment_list')
-
-from django.template.loader import render_to_string
-from django.http import JsonResponse
 
 @login_required
 def appointment_table_partial(request):
@@ -180,7 +162,6 @@ def appointment_table_partial(request):
             'appointments': appointments
         })
     })
-
 
 def dashboard_view(request):
     date_str = request.GET.get('date')
